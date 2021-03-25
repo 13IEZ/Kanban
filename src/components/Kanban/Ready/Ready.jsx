837c941ca70c9a62f94e8087d0b6ecb895e86296
Igor Zakharov <zahik1311@gmail.com>
2021-03-25 23:28:17 +0600

    Настроена логика получения id по кликнотому элементу с autocomplete

--- src/components/Kanban/Ready/Ready.jsx
+++ src/components/Kanban/Ready/Ready.jsx
@@ -1,35 +1,34 @@
-import {useEffect, useState} from 'react';
-import {useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import ReadyItem from './ReadyItem/ReadyItem';
 
-
 const Ready = () => {
-	const [disable, setDisable] = useState(true);
-	const backlogTasks = useSelector(state => state.backlogTasks);
-	const readyTasks = useSelector(state => state.readyTasks);
+  const [disable, setDisable] = useState(true);
+  const backlogTasks = useSelector((state) => state.backlogTasks);
+  const readyTasks = useSelector((state) => state.readyTasks);
 
-	useEffect(() => {
-		if (backlogTasks.length > 0) setDisable(false);
-		else setDisable(true);
-	}, [backlogTasks]);
+  useEffect(() => {
+    if (backlogTasks.length > 0) setDisable(false);
+    else setDisable(true);
+  }, [backlogTasks]);
 
-	const moveTaskHandler = () => {
+  const moveTaskHandler = () => {};
 
-	}
-
-	return (
-		<div className='Kanban-item'>
-			<h2 className='Kanban-item__title'>Ready</h2>
-			{readyTasks.map(elem => <ReadyItem key={elem.id} elem={elem}/>)}
-			{/*<ReadyItem/>*/}
-			<button
-				// onClick={console.log(1)}
-				disabled={disable}
-				className='Kanban-item__add-btn'>
-				Add card
-			</button>
-		</div>
-	);
+  return (
+    <div className='Kanban-item'>
+      <h2 className='Kanban-item__title'>Ready</h2>
+      {readyTasks.map((elem) => (
+        <ReadyItem key={elem.id} elem={elem} />
+      ))}
+      <ReadyItem />
+      <button
+        // onClick={console.log(1)}
+        disabled={disable}
+        className='Kanban-item__add-btn'>
+        Add card
+      </button>
+    </div>
+  );
 };
 
 export default Ready;
--- src/components/Kanban/Ready/ReadyItem/ReadyItem.jsx
+++ src/components/Kanban/Ready/ReadyItem/ReadyItem.jsx
@@ -1,22 +1,27 @@
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
-import './ReadyItem.scss'
-import {useSelector} from 'react-redux';
+import './ReadyItem.scss';
+import { useSelector } from 'react-redux';
 
-const ReadyItem	= () => {
-	const backlogTasks = useSelector(state => state.backlogTasks)
+const ReadyItem = () => {
+  const backlogTasks = useSelector((state) => state.backlogTasks);
 
-	return (
-		<div className="ReadyItem">
-			<Autocomplete
-				id="combo-box-demo"
-				className="ReadyItem__autocomplete"
-				options={backlogTasks}
-				getOptionLabel={(option) => option.title}
-				renderInput={(params) => <TextField {...params} variant="outlined" />}
-			/>
-		</div>
-	)
-}
+  return (
+    <div className='ReadyItem'>
+      <Autocomplete
+        id='combo-box-demo'
+        freeSolo={true}
+        disabled={false}
+        className='ReadyItem__autocomplete'
+        options={backlogTasks}
+        getOptionLabel={(option) => option.title}
+        renderInput={(params) => (
+          <TextField {...params} variant='outlined' multiline />
+        )}
+        onChange={(event, value) => console.log(value.id)}
+      />
+    </div>
+  );
+};
 
 export default ReadyItem;
